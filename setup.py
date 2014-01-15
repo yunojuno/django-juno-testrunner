c85709a242233dad03fdaaa67e8526899260d675
Steve Jalim <steve@somefantastic.co.uk>
2014-01-15 16:56:58 +0000

    Lay in .gitignore, start setup.py and README.md

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-juno-testrunner',
+    version='0.1',
+    description='A more useful (and slightly more glamorous) test runner for Django 1.6+ from the folks at YunoJuno',
+    long_description=README,
+    author='Steve Jalim, Hugo Rodger-Brown',
+    author_email='steve@somefantastic.co.uk, hugo@yunojuno.com',
+    url='https://github.com/yunojuno/django-juno-testrunner.git',
+    license='MIT',
+    packages=['help_pages'],
+    install_requires=['colorama'],
+    extras_require={'junorunner': ['colorama', ]},
+    classifiers=[
+            'Environment :: Web Environment',
+            'Framework :: Django',
+            'Framework :: Django :: 1.6',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License', # example license
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            # replace these appropriately if you are using Python 3
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Internet :: WWW/HTTP',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        ]
+)
