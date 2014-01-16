f938c44be6f004b3aaa6de5e41cb0e1c9f4112a1
Hugo Rodger-Brown <hugo@rodger-brown.com>
2014-01-16 10:31:18 +0000

    Convert readme from markdown to rst (for PyPI).

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import os
 from setuptools import setup
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-juno-testrunner',
-    version='0.1',
+    version='0.2',
     description='A more useful (and slightly more glamorous) test runner for Django 1.6+ from the folks at YunoJuno',
     long_description=README,
     author='Steve Jalim, Hugo Rodger-Brown',
