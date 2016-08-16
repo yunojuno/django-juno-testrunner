7d5c3e15bbb5e41b738cdfa256d81a7d7a3e6b17
Eleni Lixourioti <contact@eleni.co>
2016-08-16 17:31:26 +0100

    Add support for --parallel flag for Django 1.9+
    This required fixing how we get the total test count.
    
    When running tests in parallel, a core suite is generated to
    contain a collection of sub-suites that are then ran in parallel.
    
    In that case we have to go and retrieve the count from each
    of the sub-suites as the parent will not directly contain the tests.

--- junorunner/runner.py
+++ junorunner/runner.py
@@ -7,11 +7,22 @@ class JunoDiscoverRunner(DiscoverRunner):
     The only real difference between this and the standard DiscoverRunner in Django 1.6+
     is the use of the custom TextTestRunner, which we hook in via run_suite()
     """
+    def get_test_count(self, suite):
+        """
+        When running tests in parallel, a core suite is generated to contain a collection of
+        sub-suites that are then ran in parallel.
+
+        In that case we have to go and retrieve the count from each of the sub-suites as
+        the parent will not directly contain the tests.
+        """
+        if getattr(self, 'parallel', 1) > 1:
+            return sum([len(subsuite._tests) for subsuite in suite.subsuites])
+        return len(suite._tests)
 
     def run_suite(self, suite, **kwargs):
         return TextTestRunner(
             verbosity=self.verbosity,
             failfast=self.failfast,
-            total_tests=len(suite._tests),
+            total_tests=self.get_test_count(suite),
             slow_test_count=self.slow_test_count
         ).run(suite)
