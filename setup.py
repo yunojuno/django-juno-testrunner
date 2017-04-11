660735e3bed54e5d9688d02e1d641fa0289b0638
Hugo Rodger-Brown <hugo@yunojuno.com>
2017-04-11 11:38:49 +0100

    Add support for 1.11, remove <1.8
    Now that we have 1.11 as the new LTS version of Django,
    we are dropping support for anything below 1.8 (the last LTS).
    That doesn't mean it won't work just that we aren't explicitly
    testing for it.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
+from __future__ import unicode_literals
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
@@ -8,19 +9,25 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-juno-testrunner',
-    version='0.4.1',
-    description='A more useful (and slightly more glamorous) test runner for Django 1.6+ from the folks at YunoJuno',
+    version='0.4.2',
+    description='Django (1.8+) test runner with added (colour) output.',
     long_description=README,
-    author='Steve Jalim, Hugo Rodger-Brown',
-    author_email='steve@somefantastic.co.uk, hugo@yunojuno.com',
+    author='YunoJuno',
+    author_email='code@yunojuno.com',
+    maintainer='YunoJuno',
+    maintainer_email='code@yunojuno.com',
     url='https://github.com/yunojuno/django-juno-testrunner.git',
     license='MIT',
-    packages=['junorunner'],
+    packages=find_packages(),
     install_requires=['colorama'],
     extras_require={'junorunner': ['colorama', ]},
     classifiers=[
             'Environment :: Web Environment',
             'Framework :: Django',
+            'Framework :: Django :: 1.8',
+            'Framework :: Django :: 1.9',
+            'Framework :: Django :: 1.10',
+            'Framework :: Django :: 1.11',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: MIT License',
             'Operating System :: OS Independent',
@@ -28,8 +35,8 @@ setup(
             'Programming Language :: Python :: 2',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.6',
             'Topic :: Internet :: WWW/HTTP',
             'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        ]
+    ]
 )
